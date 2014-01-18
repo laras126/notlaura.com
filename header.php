e18c9aa1de454cdaf33fec5aa1e70745b2061ff6
laras126 <laras126@gmail.com>
2014-01-18 13:41:51 -0500

    add prism.js

--- header.php
+++ header.php
@@ -38,8 +38,19 @@
 			  // TODO: put it there ?>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 		
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-12198561-4']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 
 	</head>
 

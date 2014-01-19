125419e47604d5617e325d05f1cd19bd817b60b1
laras126 <laras126@gmail.com>
2014-01-18 22:02:28 -0500

    minify images

--- library/gruntfile.js
+++ library/gruntfile.js
@@ -21,18 +21,6 @@ module.exports = function(grunt) {
             }
         },
 
-        // imagemin: {
-        //     dynamic: {
-        //         files: [{
-        //             expand: true,
-        //             cwd: 'images/',
-        //             src: ['**/*.{png,jpg,gif}'],
-        //             dest: 'images/build/'
-        //         }]
-        //     }
-        // }
-
-
         compass: {
             dist: {
                 options: {
@@ -40,6 +28,38 @@ module.exports = function(grunt) {
                     cssDir: 'css',
                     require: 'susy',
                 }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        svgmin: {        
+
+            options: {                                plugins: [
+                  { removeViewBox: false },
+                  { removeUselessStrokeAndFill: false }
+                ]
+            },
+
+            dist: {                                         
+                files: {                                    
+                    // I am a tard and can't figure out file paths so am 
+                    // just listing the files since there aren't many of them
+                   'images/build/site/svg/we-fish.svg': 'images/site/svg/we-fish.svg',
+                   'images/build/site/svg/you-fish.svg': 'images/site/svg/you-fish.svg',
+                   'images/build/site/svg/me-fish.svg': 'images/site/svg/me-fish.svg',
+                   'images/build/site/svg/deliver-fish.svg': 'images/site/svg/deliver-fish.svg',
+                }
+
             }
         },
 
@@ -70,8 +90,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-svgmin');
     
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['svgmin']);
 
     
 

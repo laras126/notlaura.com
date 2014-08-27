ed73e6965f5acaff9d0035ccf1c029a51f233150
laras126 <laras126@gmail.com>
2014-08-27 10:13:33 -0400

    remove not better typekit loading

--- header.php
+++ header.php
@@ -30,19 +30,7 @@
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<script>
-		// Better loading for Typekit
-		// https://blog.5apps.com/2014/02/21/using-typekit-the-right-way-with-an-improved-loading-script.html
-		(function(d) {
-		  var tkTimeout=3000;
-		  if(window.sessionStorage){if(sessionStorage.getItem('useTypekit')==='false'){tkTimeout=0;}}
-		  var config = {
-		    kitId: 'xqo8pio',
-		    scriptTimeout: tkTimeout
-		  },
-		  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+"wf-inactive";if(window.sessionStorage){sessionStorage.setItem("useTypekit","false")}},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+="wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-		})(document);
-		</script>
+		<script>try{Typekit.load();}catch(e){}</script>
 
 		<?php wp_head(); ?>
 

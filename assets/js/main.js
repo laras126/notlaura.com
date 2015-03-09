e9966f7cd15b631a643761eb39b66c92235da685
laras126 <laras126@gmail.com>
2015-03-09 12:43:38 -0400

    working accordian and acf conditional

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,30 @@ $(document).ready(function() {
 		
 
 
+  function close_accordion_section() {
+        $('.accordion .acc-link').removeClass('active');
+        $('.accordion .acc-content').slideUp(300).removeClass('open');
+    }
+ 
+    $('.acc-link').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+ 
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        } else {
+            close_accordion_section();
+ 
+            // Add active class to section title
+            $(this).addClass('active');
+
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+ 
+        e.preventDefault();
+    });
+
 
 
 	// ----

9e522af9d95c9a5531755100282922faccbebfd0
laras126 <laras126@gmail.com>
2015-06-14 18:12:34 -0400

    add tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,54 +17,6 @@ module.exports = function(grunt) {
     require('./tasks/svgs.js')(grunt, config);
 
 
-    // require('./grunt_tasks/build-files.js')(grunt, config);
-
-
-    // Register new tasks
-
-
-    // grunt.initConfig({
-        
-    // watch: {
-    //     sass: {
-    //         files: [
-    //             'assets/scss/*.scss',
-    //             'assets/scss/**/*.scss'
-    //         ],
-    //         tasks: ['sass:dev']
-    //     },
-
-    //     css: {
-    //       files: [
-    //             'assets/css/main.css'
-    //         ],
-    //         tasks: ['cssmin:css']  
-    //     },
-
-    //     js: {
-    //         files: [
-    //             jsFileList,
-    //             '<%= jshint.all %>'
-    //         ],
-    //         tasks: ['jshint', 'concat', 'uglify']
-    //     },
-
-    //     livereload: {
-    //         // Browser live reloading
-    //         // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-    //         options: {
-    //             livereload: false
-    //         },
-    //         files: [
-    //             'assets/css/main.css',
-    //             'assets/js/scripts.js',
-    //             'views/*.twig',
-    //             '*.php'
-    //         ]
-    //         }
-    //     }
-    // });
-
     // Register tasks
     grunt.registerTask('default', [
         'sass'

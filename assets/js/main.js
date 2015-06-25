44f5ee470433e9d1abf948b811de17bc2f188abd
laras126 <laras126@gmail.com>
2015-06-24 22:28:40 -0400

    remove widow script

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,14 +88,14 @@ $(document).ready(function() {
 	// Hack to keep out widows
 	// http://css-tricks.com/preventing-widows-in-post-titles/
    
-	$('.page-title, .page-tagline, .lead').each( function() {
-		var wordArray = $(this).text().split(" ");
-		if (wordArray.length > 3) {
-			wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-			wordArray.pop();
-	    	$(this).html(wordArray.join(" "));
-	  	}
-	});
+	// $('.page-title, .page-tagline, .lead').each( function() {
+	// 	var wordArray = $(this).text().split(" ");
+	// 	if (wordArray.length > 3) {
+	// 		wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
+	// 		wordArray.pop();
+	//     	$(this).html(wordArray.join(" "));
+	//   	}
+	// });
 
 
 	// Fit Vids

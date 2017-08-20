512f1759609b321b1aa4b4d4b94a911f2ed8afd0
laras126 <laras126@gmail.com>
2017-08-19 21:43:46 -0700

    remove depracated filter

--- functions.php
+++ functions.php
@@ -193,35 +193,6 @@
 	add_filter( 'acf/fields/wysiwyg/toolbars' , 'nl_acf_wysiwyg_toolbar'  );
 
 
-
-
-
-	// Make custom fields work with Yoast SEO (only impacts the light, but helpful!)
-	// https://imperativeideas.com/making-custom-fields-work-yoast-wordpress-seo/
-
-	if ( is_admin() ) { // check to make sure we aren't on the front end
-		add_filter('wpseo_pre_analysis_post_content', 'nl_add_custom_to_yoast');
-
-		function nl_add_custom_to_yoast( $content ) {
-			global $post;
-			$pid = $post->ID;
-
-			$custom = get_post_custom($pid);
-			unset($custom['_yoast_wpseo_focuskw']); // Don't count the keyword in the Yoast field!
-
-			foreach( $custom as $key => $value ) {
-				if( substr( $key, 0, 1 ) != '_' && substr( $value[0], -1) != '}' && !is_array($value[0]) && !empty($value[0])) {
-				  $custom_content .= $value[0] . ' ';
-				}
-			}
-
-			$content = $content . ' ' . $custom_content;
-			return $content;
-
-			remove_filter('wpseo_pre_analysis_post_content', 'nl_add_custom_to_yoast'); // don't let WP execute this twice
-		}
-	}
-
 	// Remove inline gallery styles
 	add_filter( 'use_default_gallery_style', '__return_false' );
 

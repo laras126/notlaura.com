71e2be1349f4272abbe60c88b47cd09e8784bfbc
laras126 <laras126@gmail.com>
2017-08-22 21:46:02 -0700

    svg character

--- page.php
+++ page.php
@@ -26,5 +26,6 @@ $context = Timber::get_context();
 $post = new TimberPost();
 
 $context['post'] = $post;
+$context['sidebar'] = 'pages sidebar, hi!';
 
 Timber::render(array('page-' . $post->post_name . '.twig', 'page.twig'), $context);

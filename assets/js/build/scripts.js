29d49e58a5a3817d42e11b4696d2e44c23b93095
laras126 <laras126@gmail.com>
2017-08-28 22:03:26 -0700

    update smooth scrolling

--- assets/js/build/scripts.js
+++ assets/js/build/scripts.js
@@ -223,14 +223,24 @@ jQuery(document).ready(function () {
 
 	// https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
-	$('a[href*=#]:not([href=#menu])').on('click', function () {
+	$('a[href*=#]').not('[href="#"]').not('[href="#menu"]').on('click', function () {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html,body').animate({
 					scrollTop: target.offset().top
-				}, 1000);
+				}, 1000, function () {
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) {
+						// Checking if the target was focused
+						return false;
+					} else {
+						$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					};
+				});
 				return false;
 			}
 		}

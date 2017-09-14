d24e0f7262cc1f4b588a5ddc1435a54be1740a41
laras126 <laras126@gmail.com>
2017-09-13 21:09:19 -0700

    add portfolio sidebar

---
+++ sidebar-portfolio.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ */
+
+$context = array();
+
+Timber::render('sidebar-portfolio.twig', $context);

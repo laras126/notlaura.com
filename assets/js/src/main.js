11ce893bbf9a4441f2b6d7f0ec7930bfa9bcf304
laras126 <laras126@gmail.com>
2018-01-19 08:12:11 -0800

    Apply jQuery with closure, comine reply button events into one listener

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,79 +1,73 @@
 
-console.log('Check it: https://github.com/laras126/notlaura.com');
+( function($) {
 
-// FastClick
-if ('addEventListener' in document) {
-	document.addEventListener('DOMContentLoaded', function () {
+	$(document).ready(function() {
+
+		console.log('Check it: https://github.com/laras126/notlaura.com');
+
+		// FastClick
 		FastClick.attach(document.body);
-	}, false);
-}
 
-// Toggle Menu
-// http://www.a11ymatters.com/pattern/mobile-nav/#use-a-semantic-element-for-the-toggle-button
-const $toggle = document.querySelector('#toggle'),
-			$menu = document.querySelector('#menu');
+		// Toggle Menu
+		// http://www.a11ymatters.com/pattern/mobile-nav/#use-a-semantic-element-for-the-toggle-button
+		const $toggle = document.querySelector('#toggle'),
+					$menu = document.querySelector('#menu');
 
-$toggle.addEventListener('click', function () {
-	if ($menu.classList.contains('js-open')) {
-		$menu.classList.remove('js-open');
-		$toggle.classList.remove('js-open');
-	} else {
-		$menu.classList.add('js-open');
-		$toggle.classList.add('js-open');
-	}
-});
-
-
-
-
-
-var $ = jQuery;
-
-jQuery(document).ready(function() {
-
-	// ----
-	// Smooth scrolling
-	// ----
-
-	// https://css-tricks.com/snippets/jquery/smooth-scrolling/
-	var header_ht;
-
-	if (window.innerWidth <= 690) {
-		header_ht = ($('.site-header').outerHeight());
-	} else {
-		header_ht = ($('.site-header').outerHeight())*1.5;
-	}
-
-	$('a.js-scrollTo[href*="#"]').on('click', function() {
-		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-						scrollTop: target.offset().top - header_ht
-				}, 300, function() {
-					var $target = $(target);
-					$target.focus();
-					if ($target.is(":focus")) { // Checking if the target was focused
-						return false;
-					} else {
-						$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-						$target.focus(); // Set focus again
-					};
-				});
+		$toggle.addEventListener('click', function () {
+			if ($menu.classList.contains('js-open')) {
+				$menu.classList.remove('js-open');
+				$toggle.classList.remove('js-open');
+			} else {
+				$menu.classList.add('js-open');
+				$toggle.classList.add('js-open');
 			}
+		});
+
+
+		// ----
+		// Smooth scrolling for Panels
+		// ----
+
+		// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+		var header_ht;
+
+		if (window.innerWidth <= 690) {
+			header_ht = ($('.site-header').outerHeight());
+		} else {
+			header_ht = ($('.site-header').outerHeight())*1.5;
 		}
+
+		$('a.js-scrollTo[href*="#"]').on('click', function() {
+			if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+							scrollTop: target.offset().top - header_ht
+					}, 300, function() {
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						};
+					});
+				}
+			}
+		});
+
+		// Fit Vids
+		$(".main-content").fitVids({ customSelector: 'iframe' });
+
+		// Comments
+		$('.btn-reply').on('click focus', showReplyForm);
+
+		function showReplyForm() {
+			$(this).siblings('.reply-form').css({'opacity': 1, height: 'auto'});
+		}
+
 	});
 
-	// Fit Vids
-	$(".main-content").fitVids({ customSelector: 'iframe' });
-
-	$('.btn-reply').on('click', showReplyForm);
-	$('.btn-reply').on('focus', showReplyForm);
-
-	function showReplyForm() {
-		$(this).siblings('.reply-form').css({'opacity': 1, height: 'auto'});
-	}
-
-});
-
+})(jQuery);

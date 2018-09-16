95c23ba6b1872025524caa6626ce32a1880efc04
laras126 <laras126@gmail.com>
2018-09-15 18:03:40 -0700

    Add array check around series query

--- single.php
+++ single.php
@@ -11,26 +11,26 @@
 
 $post = Timber::query_post();
 $series = get_the_terms($post, 'series');
-$series_args = array(
-	'post_type' => 'post',
-	'numberposts' => -1,
-	'orderby'     => 'date',
-	'order'       => 'ASC',
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'series',
-			'field' => 'id',
-			'terms' => $series[0]->term_id
-		)
-	)
-);
 
 $context = Timber::get_context();
 $context['post'] = $post;
 $context['categories'] = Timber::get_terms('category', array('parent' => 0));
 $context['wp_title'] .= ' - ' . $post->title();
 
-if( count($series) > 0 ) {
+if( is_array( $series ) ) {
+	$series_args = array(
+		'post_type' => 'post',
+		'numberposts' => -1,
+		'orderby'     => 'date',
+		'order'       => 'ASC',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'series',
+				'field' => 'id',
+				'terms' => $series[0]->term_id
+			)
+		)
+	);
 	$context['series'] = $series[0];
 	$context['series_posts'] = Timber::get_posts($series_args);
 }
